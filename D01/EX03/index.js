5cef08a13181d3affdb7b02fed6f96a2fb390862
Arthur Azoubel <arthurazoubel@gmail.com>
2022-06-19 20:47:48 +0300

    Daily projects

--- D01/EX03/index.js
+++ D01/EX03/index.js
@@ -1,6 +1,14 @@
-function compareNumbers() {
-  //your code
+function compareNumbers(number1, number2) {
+  if (number1 < number2) {
+    console.log(`The number ${number1} is less than ${number2}`)
+  } else if (number1 > number2) {
+    console.log(`The number ${number1} is bigger than ${number2}`)
+  } else {
+    console.log(`The number ${number1} is equal to ${number2}`)
+  }
 }
+
+compareNumbers(4,4)
 
 // Do not remove or change this line, or the tests won't work
 export { compareNumbers };
