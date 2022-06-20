ff5312f4b476205bdc9542f16f4d645ccc5ae909
Arthur Azoubel <arthurazoubel@gmail.com>
2022-06-20 15:02:26 +0300

    June 20 Commit

--- D01/EX04/index.js
+++ D01/EX04/index.js
@@ -1,6 +1,19 @@
-function modulo() {
-  //your code
+function modulo(number1, number2) {
+  if (number1 < number2) {
+    console.log(`The number ${number1} is less than ${number2}`)
+  } else if (number1 > number2) {
+      var modulo = number1 % number2;
+      if (modulo > 0) {
+        console.log(`The number ${number1} is bigger than ${number2}`)
+        console.log(`The modulo of ${number1} % ${number2} is ${modulo}`)
+      } else {
+        console.log(`The number ${number1} is bigger than ${number2}`)
+      }
+  } else {
+    console.log(`The number ${number1} is equal to ${number2}`)
+  }
 }
 
+modulo(8,3)
 // Do not remove or change this line, or the tests won't work
 export { modulo };
